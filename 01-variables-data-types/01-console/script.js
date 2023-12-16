b2d6ff58117f4a3632f7085bc2b2b84cff80b9e1
Jennifer <adaeze.eth@gmail.com>
2023-12-16 06:05:41 +0100

    my 4th command

--- 01-variables-data-types/01-console/script.js
+++ 01-variables-data-types/01-console/script.js
@@ -1,9 +1,20 @@
-function fetchUser() {
-  fetch('https://randomuser.me/api')
-    .then((res)) => res.json())
-    .then((data) => {
-console.log(data);
-   });
+try {
+ console.log(x);
+} catch (error) {
+  console.log('Error: '+ error);
+}
+
+function double(number) {
+  if (isNaN(number)) {
+    throw new Error(number + ' is not a number');
   }
 
-fetchUser();
+  return number * 2;
+}
+
+try {
+  const y = double('hello');
+  console.log(y);
+} catch(error) {
+  console.log(error);
+}
